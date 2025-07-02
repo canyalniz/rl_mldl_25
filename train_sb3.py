ac90b936a6ca459f7fe297c1c45bcfaa8bca4ec7
canyalniz <canyalniz97@gmail.com>
2025-07-02 15:30:47 +0200

    Getting started with PPO. Imported zoo hyperparameters but in the end the default ones worked much better. Will think about tuning later.

--- train_sb3.py
+++ train_sb3.py
@@ -6,13 +6,62 @@
 """
 import gym
 from env.custom_hopper import *
+import torch
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import SubprocVecEnv
 
 def main():
-    train_env = gym.make('CustomHopper-source-v0')
+    # # train_env = gym.make('CustomHopper-source-v0')
 
-    print('State space:', train_env.observation_space)  # state-space
-    print('Action space:', train_env.action_space)  # action-space
-    print('Dynamics parameters:', train_env.get_parameters())  # masses of each link of the Hopper
+    # Parallel environments
+    vec_env = make_vec_env("CustomHopper-source-v0", n_envs=8, vec_env_cls=SubprocVecEnv)
+
+    print('State space:', vec_env.observation_space)  # state-space
+    print('Action space:', vec_env.action_space)  # action-space
+    # print('Dynamics parameters:', vec_env.get_parameters())  # masses of each link of the Hopper
+
+    hyperparams = dict(
+            # env_wrapper=[{"gymnasium.wrappers.TimeLimit": {"max_episode_steps": 100}}],
+            normalize_advantage=True,
+            # n_envs=1,
+            policy="MlpPolicy",
+            batch_size=32,
+            n_steps=512,
+            gamma=0.999,
+            learning_rate=9.80828e-05,
+            ent_coef=0.00229519,
+            clip_range=0.2,
+            n_epochs=5,
+            gae_lambda=0.99,
+            max_grad_norm=0.7,
+            vf_coef=0.835671,
+            # use_sde=True,
+            policy_kwargs=dict(
+                log_std_init=-2,
+                ortho_init=False,
+                activation_fn=torch.nn.ReLU,
+                net_arch=dict(pi=[256, 256], vf=[256, 256])
+            ),
+            verbose=1,
+            device="cpu",
+    )
+
+    # model = PPO(env=vec_env, **hyperparams)
+    model = PPO(policy="MlpPolicy", env=vec_env, verbose=1, device="cpu")
+    model.learn(total_timesteps=5e6, progress_bar=True)
+    model.save("ppo_hopper_5e6")
+
+    del model # remove to demonstrate saving and loading
+
+    model = PPO.load("ppo_hopper_5e6")
+
+    obs = vec_env.reset()
+    while True:
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = vec_env.step(action)
+        vec_env.render("human")
 
     #
     # TASK 4 & 5: train and test policies on the Hopper env with stable-baselines3
