51ec9b666a691ad9108ca6667cc04dc7c0fb8334
canyalniz <canyalniz97@gmail.com>
2025-07-05 12:50:07 +0200

    Optuna script for hyperparameter tuning.

--- optuna_sb3.py
+++ optuna_sb3.py
@@ -1,182 +1,76 @@
-"""Optuna example that optimizes the hyperparameters of
-a reinforcement learning agent using PPO implementation from Stable-Baselines3
-on a gym environment.
-
-This is a simplified version of what can be found in https://github.com/DLR-RM/rl-baselines3-zoo.
-
-You can run this example as follows:
-    $ python sb3_simple.py
-
-"""
-
-from typing import Any
-from typing import Dict
-
-import gym
 import optuna
-from optuna.pruners import MedianPruner
-from optuna.samplers import TPESampler
+import gym
 from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.vec_env import VecMonitor
 from stable_baselines3.common.monitor import Monitor
-import torch
-import torch.nn as nn
 
 from env.custom_hopper import *
 
+def make_vec_train_env():
+    # Create a vector environment
+    train_env = make_vec_env("CustomHopper-source-v0", n_envs=8, vec_env_cls=SubprocVecEnv)
+    # Use the Monitor wrapper to record experiment results
+    train_env = VecMonitor(train_env)
+    return train_env
 
-N_TRIALS = 100
-N_STARTUP_TRIALS = 5
-N_EVALUATIONS = 2
-N_TIMESTEPS = int(3e5)
-EVAL_FREQ = int(N_TIMESTEPS / N_EVALUATIONS)
-N_EVAL_EPISODES = 3
+def make_env():
+    return Monitor(gym.make("CustomHopper-source-v0"))
 
-ENV_ID = "CustomHopper-source-v0"
-
-DEFAULT_HYPERPARAMS = {
-    "policy": "MlpPolicy",
-    "env": ENV_ID,
-}
-
-
-def sample_ppo_params(trial: optuna.Trial) -> Dict[str, Any]:
-    """Sampler for PPO hyperparameters."""
+def optimize_ppo(trial):
+    # Sample hyperparameters
     learning_rate = trial.suggest_float("lr", 1e-5, 1e-3)
-    n_steps = 2 ** trial.suggest_int("exponent_n_steps", 9, 12)
-    vf_coef = trial.suggest_float("vf_coef", 0.45, 0.9, log=True)
-    ent_coef = trial.suggest_float("ent_coef", 0, 0.005)
-    net_arch = trial.suggest_categorical("net_arch", ["tiny", "small"])
-    activation_fn = trial.suggest_categorical("activation_fn", ["tanh", "relu"])
-    ortho_init = trial.suggest_categorical("ortho_init", [False, True])
-    max_grad_norm = trial.suggest_float("max_grad_norm", 0.3, 1.0, log=True)
+    n_steps = 2**trial.suggest_int("n_steps_exponent", 9,12)
     gamma = 1.0 - trial.suggest_float("gamma", 0.0001, 0.01, log=True)
     gae_lambda = 1.0 - trial.suggest_float("gae_lambda", 0.001, 0.1, log=True)
+    ent_coef = trial.suggest_float("ent_coef", 1e-6, 0.005, log=True)
+    clip_range = trial.suggest_float("clip_range", 0.1, 0.3)
+    vf_coef = trial.suggest_float("vf_coef", 0.45, 0.9, log=True)
 
-    # Display true values.
-    # trial.set_user_attr("gamma_", gamma)
-    # trial.set_user_attr("gae_lambda_", gae_lambda)
-    trial.set_user_attr("n_steps", n_steps)
+    train_env = make_vec_train_env()
+    test_env = DummyVecEnv([make_env])
 
-    net_arch = [
-        {"pi": [64], "vf": [64]} if net_arch == "tiny" else {"pi": [64, 64], "vf": [64, 64]}
-    ]
-
-    activation_fn = {"tanh": nn.Tanh, "relu": nn.ReLU}[activation_fn]
-
-    return {
-        "n_steps": n_steps,
-        "learning_rate": learning_rate,
-        "gamma": gamma,
-        "gae_lambda": gae_lambda,
-        "vf_coef": vf_coef,
-        "ent_coef": ent_coef,
-        "max_grad_norm": max_grad_norm,
-        "policy_kwargs": {
-            "net_arch": net_arch,
-            "activation_fn": activation_fn,
-            "ortho_init": ortho_init,
-        },
-    }
-
-
-class TrialEvalCallback(EvalCallback):
-    """Callback used for evaluating and reporting a trial."""
-
-    def __init__(
-        self,
-        eval_env: gym.Env,
-        trial: optuna.Trial,
-        n_eval_episodes: int = 5,
-        eval_freq: int = 10000,
-        deterministic: bool = True,
-        verbose: int = 0,
-    ):
-        super().__init__(
-            eval_env=eval_env,
-            n_eval_episodes=n_eval_episodes,
-            eval_freq=eval_freq,
-            deterministic=deterministic,
-            verbose=verbose,
-        )
-        self.trial = trial
-        self.eval_idx = 0
-        self.is_pruned = False
-
-    def _on_step(self) -> bool:
-        if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
-            super()._on_step()
-            self.eval_idx += 1
-            self.trial.report(self.last_mean_reward, self.eval_idx)
-            # Prune trial if need.
-            if self.trial.should_prune():
-                self.is_pruned = True
-                return False
-        return True
-
-
-def objective(trial: optuna.Trial) -> float:
-    kwargs = DEFAULT_HYPERPARAMS.copy()
-    # Sample hyperparameters.
-    kwargs.update(sample_ppo_params(trial))
-    # Create the RL model.
-    model = PPO(**kwargs)
-    # Create env used for evaluation.
-    eval_env = Monitor(gym.make(ENV_ID))
-    # Create the callback that will periodically evaluate and report the performance.
-    eval_callback = TrialEvalCallback(
-        eval_env, trial, n_eval_episodes=N_EVAL_EPISODES, eval_freq=EVAL_FREQ, deterministic=True
+    model = PPO(
+        "MlpPolicy",
+        train_env,
+        verbose=0,
+        tensorboard_log=None,
+        learning_rate=learning_rate,
+        n_steps=n_steps,
+        gamma=gamma,
+        gae_lambda=gae_lambda,
+        ent_coef=ent_coef,
+        clip_range=clip_range,
+        vf_coef=vf_coef,
+        device="cpu",
     )
 
-    nan_encountered = False
-    try:
-        model.learn(N_TIMESTEPS, callback=eval_callback)
-    except AssertionError as e:
-        # Sometimes, random hyperparams can generate NaN.
-        print(e)
-        nan_encountered = True
-    finally:
-        # Free memory.
-        model.env.close()
-        eval_env.close()
+    # Short training for quick evaluation
+    model.learn(total_timesteps=int(8e5))
+    train_env.close()
 
-    # Tell the optimizer that the trial failed.
-    if nan_encountered:
-        return float("nan")
+    # Evaluate the model
+    mean_reward, _ = evaluate_policy(model, test_env, n_eval_episodes=5, deterministic=True)
+    test_env.close()
 
-    if eval_callback.is_pruned:
-        raise optuna.exceptions.TrialPruned()
-
-    return eval_callback.last_mean_reward
-
+    return mean_reward
 
 if __name__ == "__main__":
-    # Set pytorch num threads to 1 for faster training.
-    torch.set_num_threads(1)
+    study = optuna.create_study(
+        study_name="ppo_source_study",
+        direction="maximize",
+        storage="sqlite:///ppo_source_study.db",
+        load_if_exists=True
+    )
 
-    sampler = TPESampler(n_startup_trials=N_STARTUP_TRIALS)
-    # Do not prune before 1/3 of the max budget is used.
-    pruner = MedianPruner(n_startup_trials=N_STARTUP_TRIALS, n_warmup_steps=N_EVALUATIONS // 3)
-
-    storage_url = "sqlite:///ppo_source_study.db"
-
-    study = optuna.create_study(sampler=sampler, pruner=pruner, direction="maximize", storage=storage_url, load_if_exists=True)
     try:
-        study.optimize(objective, n_trials=N_TRIALS, timeout=600, show_progress_bar=True)
+        study.optimize(optimize_ppo, n_trials=13, gc_after_trial=True, show_progress_bar=True)
     except KeyboardInterrupt:
         pass
-
-    print("Number of finished trials: ", len(study.trials))
+    
 
     print("Best trial:")
-    trial = study.best_trial
-
-    print("  Value: ", trial.value)
-
-    print("  Params: ")
-    for key, value in trial.params.items():
-        print("    {}: {}".format(key, value))
-
-    print("  User attrs:")
-    for key, value in trial.user_attrs.items():
-        print("    {}: {}".format(key, value))
+    print(study.best_trial)
