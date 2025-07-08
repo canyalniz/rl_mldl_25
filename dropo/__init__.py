67910f2042ea2fd5032d30aa2d3cb2f9e6101f95
canyalniz <canyalniz97@gmail.com>
2025-07-08 12:46:55 +0200

    Including DROPO with the necessary fixes.

---
+++ dropo/__init__.py
@@ -0,0 +1,18 @@
+"""
+Taken from the official DROPO repository https://github.com/gabrieletiboni/dropo
+This file is included in the main project repository to be able to submit a self contained code base.
+
+@article{tiboni2023dropo,
+title = {DROPO: Sim-to-real transfer with offline domain randomization},
+journal = {Robotics and Autonomous Systems},
+pages = {104432},
+year = {2023},
+issn = {0921-8890},
+doi = {https://doi.org/10.1016/j.robot.2023.104432},
+url = {https://www.sciencedirect.com/science/article/pii/S0921889023000714},
+author = {Gabriele Tiboni and Karol Arndt and Ville Kyrki},
+keywords = {Robot learning, Transfer learning, Reinforcement learning, Domain randomization}
+}
+"""
+
+from .dropo import Dropo
