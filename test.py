254ce1422f292a2a844372cb85a11c3e31debb7d
canyalniz <canyalniz97@gmail.com>
2025-07-09 21:23:42 +0200

    Updated test script

--- test.py
+++ test.py
@@ -1,18 +1,28 @@
 """Test an RL agent on the OpenAI Gym Hopper environment"""
 import argparse
+import os
 
 import torch
 import gym
 
 from env.custom_hopper import *
-from agent import Agent, Policy
+from agent import Agent, Policy, ValueEstimator
+import pandas as pd
+
+def positive_int(x):
+   x = int(x)
+   if x <= 0:
+      raise argparse.ArgumentTypeError("Needs to be positive")
+   return x
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', default=None, type=str, help='Model path')
-    parser.add_argument('--device', default='cpu', type=str, help='network device [cpu, cuda]')
-    parser.add_argument('--render', default=False, action='store_true', help='Render the simulator')
-    parser.add_argument('--episodes', default=10, type=int, help='Number of test episodes')
+    parser.add_argument('--run-id', type=str, help='ID corresponding to the run to identify the run directory', required=True)
+    parser.add_argument('--n-episodes', default=50, type=positive_int, help='Number of episodes to evaluate the agent')
+    parser.add_argument('--eval-env', default='source', type=str, help='Training environment [source, target, source-udr]')
+    parser.add_argument('--train-env', default='source', type=str, help='Training environment [source, target, source-udr]')
+    parser.add_argument('--logs-models-path', default='logs_and_models', type=str, help='Path to the logs_and_models directory')
+    parser.add_argument('--model-name', default='best_model', type=str, help='Name of the model in the run directory')
 
     return parser.parse_args()
 
@@ -20,40 +30,61 @@ args = parse_args()
 
 
 def main():
+    envs = {
+         "source":"CustomHopper-source-v0",
+         "target":"CustomHopper-target-v0",
+         "source-udr":"CustomHopper-source-UDR-v0"
+         }
+    run_dir = os.path.join(args.logs_models_path, args.run_id)
 
-	env = gym.make('CustomHopper-source-v0')
-	# env = gym.make('CustomHopper-target-v0')
+    env = gym.make(envs[args.eval_env])
 
-	print('Action space:', env.action_space)
-	print('State space:', env.observation_space)
-	print('Dynamics parameters:', env.get_parameters())
-	
-	observation_space_dim = env.observation_space.shape[-1]
-	action_space_dim = env.action_space.shape[-1]
+    observation_space_dim = env.observation_space.shape[-1]
+    action_space_dim = env.action_space.shape[-1]
 
-	policy = Policy(observation_space_dim, action_space_dim)
-	policy.load_state_dict(torch.load(args.model), strict=True)
+    model_name = args.model_name
+    
+    policy = Policy(observation_space_dim, action_space_dim)
+    policy.load_state_dict(torch.load(os.path.join(run_dir, model_name + "_policy.mdl")), strict=True)
 
-	agent = Agent(policy, device=args.device)
+    value_function = None
+    if "critic" in model_name:
+        value_function = ValueEstimator(observation_space_dim)
+        value_function.load_state_dict(torch.load(os.path.join(run_dir, model_name + "_value_function.mdl")), strict=True)
 
-	for episode in range(args.episodes):
-		done = False
-		test_reward = 0
-		state = env.reset()
+    agent = Agent(policy=policy, value_function=value_function, run_id=args.run_id)
 
-		while not done:
+    eval_records_path = os.path.join(run_dir, "eval_records.csv")
+    try:
+        eval_records = pd.read_csv(eval_records_path)
+    except OSError:
+        print("No existing evaluation records found for this run. Creating new records.")
+        eval_records = pd.DataFrame(columns=["model", "returns_mean", "returns_std", "eval_env", "train_env"])
 
-			action, _ = agent.get_action(state, evaluation=True)
 
-			state, reward, done, info = env.step(action.detach().cpu().numpy())
+    rewards = np.zeros(args.n_episodes)
+    for episode in range(args.n_episodes):
+        done = False
+        test_reward = 0
+        state = env.reset()
 
-			if args.render:
-				env.render()
+        while not done:
 
-			test_reward += reward
+            action, _ = agent.get_action(state, evaluation=True)
 
-		print(f"Episode: {episode} | Return: {test_reward}")
-	
+            state, reward, done, info = env.step(action.detach().cpu().numpy())
+
+            test_reward += reward
+
+        rewards[episode] = test_reward
+    
+    returns_mean = np.mean(rewards)
+    returns_std = np.std(rewards)
+
+    new_row = pd.DataFrame([[args.model_name, returns_mean, returns_std, args.eval_env, args.train_env]], columns=["model", "returns_mean", "returns_std", "eval_env", "train_env"])
+    eval_records = pd.concat([eval_records, new_row], ignore_index=True)
+    eval_records.to_csv(eval_records_path, index=False)
+
 
 if __name__ == '__main__':
-	main()
+    main()
