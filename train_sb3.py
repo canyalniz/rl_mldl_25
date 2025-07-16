0e5b13eac9fc62e6ad7a23d6c4e11328879939e3
canyalniz <canyalniz97@gmail.com>
2025-07-17 00:34:38 +0200

    Switching to a more compatible model and log saving scheme. Switching over to EvalCallback as recommended by sb3

--- train_sb3.py
+++ train_sb3.py
@@ -22,7 +22,7 @@ from stable_baselines3.common import results_plotter
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import VecMonitor
 from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
-from stable_baselines3.common.callbacks import BaseCallback
+from modified_sb3.common.callbacks import EvalCallback
 
 def positive_int(x):
    x = int(x)
@@ -32,77 +32,35 @@ def positive_int(x):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--timesteps', default=1000000, type=positive_int, help='Number of training timesteps')
-    parser.add_argument('--check-freq', default=1000, type=positive_int, help='Wait for this many timesteps before checking to see if you should save the model')
-    parser.add_argument('--skip-over', default=750000, type=positive_int, help='Wait for this many timesteps before checking to see if you should save the model')
-    parser.add_argument('--n-envs', default=8, type=positive_int, help='Number of environments to run in parallel')
+    parser.add_argument('--timesteps', default=1600000, type=positive_int, help='Number of training timesteps')
+    parser.add_argument('--eval-freq', default=1000, type=positive_int, help='Timesteps between evaluating the model')
+    parser.add_argument('--eval-wait', default=800000, type=positive_int, help='Wait for this many timesteps before starting to evaluate the model')
+    parser.add_argument('--n-envs', default=8, type=positive_int, help='Number of environments to run in parallel in the vector environment')
     parser.add_argument('--device', default='cpu', type=str, help='Network device [cpu, cuda]')
-    parser.add_argument('--env', default='source', type=str, help='Training environment [source, target, source-udr]')
-    parser.add_argument('--model-name', default='best_model', type=str, help='Model will be saved by this name in the run directory corresponding to the id')
+    parser.add_argument('--env', default='source', type=str, help='Training environment [source, target, source-udr, source-normal]')
+    parser.add_argument('--eval-env', default='source', type=str, help='Evaluation environment [source, target, source-udr, source-normal]')
     parser.add_argument('--run-id', default=None, type=str, help='ID of the run, if no id is provided it is automatically assigned according to the timestamp')
 
     return parser.parse_args()
 
 args = parse_args()
 
-class SaveOnBestTrainingRewardCallback(BaseCallback):
-    """
-    Callback for saving a model (the check is done every ``check_freq`` steps)
-    based on the training reward (in practice, we recommend using ``EvalCallback``).
-
-    :param check_freq:
-    :param skip_over: Wait this many timesteps before evaluating models
-    :param log_dir: Path to the folder where the model will be saved.
-      It must contain the file created by the ``Monitor`` wrapper.
-    :param verbose: Verbosity level: 0 for no output, 1 for info messages, 2 for debug messages
-    """
-    def __init__(self, check_freq: int, skip_over: int, log_dir: str, verbose: int = 1):
-        super().__init__(verbose)
-        self.check_freq = check_freq
-        self.skip_over = skip_over
-        self.log_dir = log_dir
-        self.save_path = os.path.join(log_dir, args.model_name)
-        self.best_mean_reward = -np.inf
-
-    def _init_callback(self) -> None:
-        # Create folder if needed
-        if self.save_path is not None:
-            os.makedirs(self.log_dir, exist_ok=True)
-
-    def _on_step(self) -> bool:
-        if (self.n_calls>=self.skip_over) and (self.n_calls % self.check_freq == 0):
-
-          # Retrieve training reward
-          x, y = ts2xy(load_results(self.log_dir), "timesteps")
-          if len(x) > 0:
-              # Mean training reward over the last 100 episodes
-              mean_reward = np.mean(y[-100:])
-              if self.verbose >= 1:
-                print(f"Num timesteps: {self.num_timesteps}")
-                print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
-
-              # New best model, you could save the agent here
-              if mean_reward > self.best_mean_reward:
-                  self.best_mean_reward = mean_reward
-                  # Example for saving best model
-                  if self.verbose >= 1:
-                    print(f"Saving new best model to {self.save_path}")
-                  self.model.save(self.save_path)
-
-        return True
-
 def main():
+    
     # Unique tag of this run based on the timestamp
     run_tag = strftime("%Y-%m-%d--%H_%M_%S", gmtime()) if args.run_id is None else args.run_id
 
     # Create log dir belonging to this run
     run_dir = os.path.join("logs_and_models", run_tag)
+    
+    if os.path.exists(run_dir):
+       raise FileExistsError("The run already exists, refusing to overwrite, exiting...")
+    
     os.makedirs(run_dir, exist_ok=True)
 
-    if os.path.exists(os.path.join(run_dir, args.model_name)):
-       raise FileExistsError("The model already exists, refusing to overwrite, exiting...")
-
+    ## CREATE TRAINING ENVIRONMENT ##
     n_envs = args.n_envs
+    # Mapping from command-line input to source tag
     envs = {
        "source":"CustomHopper-source-v0",
        "target":"CustomHopper-target-v0",
@@ -110,29 +68,20 @@ def main():
        "source-normal":"CustomHopper-source-normal-v0"
        }
 
-    log_filename = os.path.join(run_dir, "train_monitor.csv")
+    # Path to the monitor csv to be created by Monitor Wrapper
+    train_log_filename = os.path.join(run_dir, "train_monitor.csv")
     if n_envs > 1:
       # Create a vector environment
       train_env = make_vec_env(envs[args.env], n_envs=n_envs, vec_env_cls=SubprocVecEnv)
-      # Use the Monitor wrapper to record experiment results
-      train_env = VecMonitor(train_env, log_filename)
+      # Use the VecMonitor wrapper to record experiment results
+      train_env = VecMonitor(train_env, train_log_filename)
     else:
+      # Create single vector environment
       train_env = gym.make(envs[args.env])
-      train_env = Monitor(train_env, log_filename)
+      # Use the Monitor wrapper to record experiment results
+      train_env = Monitor(train_env, train_log_filename)
 
-    # print('State space:', vec_env.observation_space)  # state-space
-    # print('Action space:', vec_env.action_space)  # action-space
-    # print('Dynamics parameters:', vec_env.get_parameters())  # masses of each link of the Hopper
-
-    # hyperparams={
-    #    'learning_rate': 0.0007492360821111548,
-    #    'n_steps': 2**11,
-    #    'gamma': 1- 0.006075826916453786,
-    #    'gae_lambda': 1 - 0.08382799471957811,
-    #    'ent_coef': 2.1645583839106794e-05,
-    #    'clip_range': 0.21302588603725742,
-    #    'vf_coef': 0.6506617414668732}
-    
+    ## SET HYPERPARAMETERS ##
     hyperparams={
        'learning_rate': 0.0006029621422146909,
        'n_steps': 2**11,
@@ -143,28 +92,39 @@ def main():
        'vf_coef': 0.5083057178072663}
 
     timesteps = args.timesteps
-
-    # Create instance of the callback that saves the best model
-    callback = SaveOnBestTrainingRewardCallback(check_freq=args.check_freq, skip_over=(args.skip_over // n_envs), log_dir=run_dir)
-
-    # model = PPO(env=vec_env, **hyperparams)
-    model = PPO(policy="MlpPolicy", env=train_env, verbose=1, device=args.device, _init_setup_model=False, **hyperparams)
     
-    # get argument names to PPO.__init__
+    ## SAVE HYPERPARAMETERS ##
+    # Create the model without initializing it, this is to get hyperparameter information
+    model = PPO(policy="MlpPolicy", env=train_env, verbose=1, device=args.device, _init_setup_model=False, **hyperparams)
+    # Get argument names to PPO.__init__
     PPO_init_arguments = PPO.__init__.__code__.co_varnames[:PPO.__init__.__code__.co_argcount]
-    # get the hyperparameter values used to initialize the model
+    # Get the hyperparameter values used to initialize the model
     hyperparams_reference = {arg:model.__dict__[arg] for arg in PPO_init_arguments if arg not in ["self", "_init_setup_model", "policy", "env"]}
     hyperparams_reference['device'] = str(hyperparams_reference['device'])
-    # save hyperparameters for reference
+    # Save hyperparameters for reference
     with open(os.path.join(run_dir, 'hyperparams.yaml'), 'w') as f:
        yaml.dump(hyperparams_reference, f)
     
+    ## MODEL CREATION ##
+    # Create the model according to the given command-line arguments and the given hyperparameters
     model = PPO(policy="MlpPolicy", env=train_env, verbose=1, device=args.device, _init_setup_model=True, **hyperparams)
-    # learn the policy
-    model.learn(total_timesteps=timesteps, progress_bar=True, callback=callback)
+    
+    ## TRAINING ##    
+    if n_envs > 1:
+      # Create a vector environment
+      eval_env = make_vec_env(envs[args.eval_env], n_envs=n_envs, vec_env_cls=SubprocVecEnv)
+    else:
+      # Create single vector environment
+      train_env = gym.make(envs[args.eval_env])
 
-    plot_results([run_dir], timesteps, results_plotter.X_TIMESTEPS, "PPO-Hopper")
-    plt.savefig(os.path.join(run_dir, "training_graph.png"), format="png")
+    eval_callback = EvalCallback(eval_env, best_model_save_path=run_dir,
+                             log_path=run_dir, eval_freq = max(args.eval_freq // n_envs, 1),
+                             wait_for=max(args.eval_wait // n_envs, 1), deterministic=True, render=False)
+    # Learn the policy
+    model.learn(total_timesteps=timesteps, progress_bar=True, callback=eval_callback)
+
+    ## PLOTTING ##
+    plot_results([run_dir], timesteps, results_plotter.X_TIMESTEPS, args.run_id)
     plt.show()
 
 
